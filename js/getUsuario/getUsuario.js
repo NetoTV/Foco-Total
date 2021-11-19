5f185fd621fbc6d487f2b4c3f99362825a89e18c
Paulo Leandro Bertotti <pauloleandrolopezzz@gmail.com>
2021-11-18 22:23:08 -0300

    Validação Login

---
+++ js/getUsuario/getUsuario.js
@@ -0,0 +1,10 @@
+// // Requisitando dados para o back-end
+// const request = new XMLHttpRequest();
+
+// let data = { id: 1 } // Aqui vai entrar o $Session do usuário
+
+// data = JSON.stringify(data)
+
+// request.open('GET', 'http://localhost:3000/usuario');
+// request.setRequestHeader("Content-Type", "application/json");
+// request.send(data);
