40d1b3b33168462cb8007d9255368ab2e47db6cb
Paulo Leandro Bertotti <pauloleandrolopezzz@gmail.com>
2021-11-19 19:12:52 -0300

    Adicionando calendário e front-end 'Criar Evento'

--- js/outros/controleHeader.js
+++ js/outros/controleHeader.js
@@ -1,8 +1,24 @@
+let currentScrollPos = window.pageYOffset;
+if (currentScrollPos < 70) {
+    document.getElementById("headerComControle").style.top = "0";
+} else if (currentScrollPos > 140) {
+    document.getElementById("headerComControle").style.top = "-200px";
+}
+
+else {
+    document.getElementById("headerComControle").style.top = "-180px";
+}
+prevScrollpos = currentScrollPos;
+
 window.onscroll = function () {
     let currentScrollPos = window.pageYOffset;
     if (currentScrollPos < 70) {
         document.getElementById("headerComControle").style.top = "0";
-    } else {
+    } else if (currentScrollPos > 140) {
+        document.getElementById("headerComControle").style.top = "-200px";
+    }
+
+    else {
         document.getElementById("headerComControle").style.top = "-180px";
     }
     prevScrollpos = currentScrollPos;
