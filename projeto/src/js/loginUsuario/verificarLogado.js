ed449f00588a215b2e55ac7e841c5adeaf2aefb1
Raphael Finatto <raphaelfinattonardelli@gmail.com>
2021-12-06 21:56:46 -0300

    Alteraçoes simples
    Somente mudando nome de paginas, fontes e etc

--- projeto/src/js/loginUsuario/verificarLogado.js
+++ projeto/src/js/loginUsuario/verificarLogado.js
@@ -27,7 +27,7 @@ const request = new XMLHttpRequest();
 request.onreadystatechange = function () {
     if (request.readyState == 4 && request.status == 200) {
         const usuario = (JSON.parse(request.response))
-        document.querySelector('.cta h3').textContent = `${usuario.emailUsuario}`
+        document.querySelector('h3').textContent = `${usuario.emailUsuario}`
     }
 }
 
